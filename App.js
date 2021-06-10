153d54d046b5fd23c20ac135da0e4fddec60b1ed
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-10 21:08:34 +0000

    added WeatherApp

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import TodoApp from "./Project/TodoApp/TodoApp";
+import Card from "./Project/Card/Card";
+import MainApp from "./Project/ExternalStyle/MainApp";
+import WeatherApp  from "./Project/Weather/WeatherApp";
+import NYT from './Project/NYT/Main'
 
 export default function App() {
-  return <TodoApp />;
+  return <WeatherApp />;
 }
