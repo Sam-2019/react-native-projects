1613dbf2af05d02a00e49ed1443ac699adc5ae95
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-10 09:05:35 +0000

    added Forecast

---
+++ Project/Weather/Forecast.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Forecast = ({ main, description, temp }) => {
+  return (
+    <View>
+      <Text style={styles.bigText}>{main}</Text>
+
+      <Text style={styles.mainText}>Current conditions: {description}</Text>
+
+      <Text style={styles.bigText}>{temp}°F</Text>
+    </View>
+  );
+};
+
+export default Forecast;
+
+const styles = StyleSheet.create({
+  container: { height: 130 },
+  bigText: {
+    flex: 2,
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "#FFFFFF",
+  },
+  mainText: { flex: 1, fontSize: 16, textAlign: "center", color: "#FFFFFF" },
+});
