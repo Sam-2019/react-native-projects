364fed29e9ff1d5ede284d9a145dc4734b05eb81
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 10:56:34 +0000

    imported new components

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import TodoApp from "./Project/TodoApp/TodoApp";
-import Card from "./Project/Card/Card";
-import MainApp from "./Project/ExternalStyle/MainApp";
-import WeatherApp  from "./Project/Weather/WeatherApp";
-import NYT from './Project/NYT/Main'
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import NavigationApp from "./Project/CitiesApp/Main";
+import store from "./store";
+import { Provider } from "react-redux";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    accent: "yellow",
+  },
+};
 
 export default function App() {
-  return <WeatherApp />;
+  return (
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <NavigationApp />
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
+  );
 }
