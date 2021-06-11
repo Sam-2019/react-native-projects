451cdaa77d7970b113584b8964b9b5203facc1e4
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 15:59:44 +0000

    added initialState

--- Project/CitiesApp/features/citySlice.js
+++ Project/CitiesApp/features/citySlice.js
@@ -2,7 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const citySlice = createSlice({
   name: "cities",
-  initialState: [],
+  initialState: [
+    { id: '123', city: 'Tokyo', country: "Japan" },
+    { id: '1234', city: 'Tema', country: "Ghana" },
+  ],
   reducers: {
     addCity: (state, action) => {
       return state.concat(action.payload);
