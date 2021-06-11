498ef9bbd1dedfa907c1260fbeaa4317e0acd3ca
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 14:56:56 +0000

    added locationReducer

--- store.js
+++ store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cityReducer from './Project/CitiesApp/features/citySlice'
+import locationReducer from './Project/CitiesApp/features/locationSlice'
 
 export default configureStore({
   reducer: {
     cities: cityReducer,
+    locations: locationReducer
   },
 });
