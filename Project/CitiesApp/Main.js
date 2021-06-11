9b1048a14acb12e3bd0dcc061b28a7315ad0b189
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 14:48:05 +0000

    removed unused imports

--- Project/CitiesApp/Main.js
+++ Project/CitiesApp/Main.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Cities from "./Cities/Cities";
 import AddCity from "./AddCity";
+import CitiesStack from "./Cities/CitiesStack";
 
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Cities" component={Cities} />
+      <Tab.Screen name="Cities" component={CitiesStack} />
       <Tab.Screen name="Add City" component={AddCity} />
     </Tab.Navigator>
   );
 };
 
 export default Main;
-
-const styles = StyleSheet.create({});
