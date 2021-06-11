fed072a4d6e31e203769c2f2d9f2fb6b2ad5ca06
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 14:50:34 +0000

    add City

---
+++ Project/CitiesApp/Cities/City.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import { citiesData } from "../features/citySlice";
+import Item from "../component/item";
+import AddLocation from "../AddLocation";
+
+function City() {
+  return (
+    <View>
+      <AddLocation />
+    </View>
+  );
+}
+
+export default City;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  empty_container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header_text: {
+    fontSize: 50,
+  },
+});
