cee7f57824227e01f4de3ff585895c9ecabf67a9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-11 11:42:30 +0000

    removed whitespace

--- Project/CitiesApp/Cities/Cities.js
+++ Project/CitiesApp/Cities/Cities.js
@@ -2,22 +2,30 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import { citiesData } from "../features/citySlice";
+import ListItem from "../ListItem";
 
 function Cities() {
   const listCities = useSelector(citiesData);
 
-  if (!listCities) {
-    return (
-      <View style={styles.empty_container}>
-        <Text>No entry yet!</Text>
-      </View>
-    );
-  }
+  // console.log(listCities)
+
+  // if (!listCities) {
+  //   return (
+  //     <View style={styles.empty_container}>
+  //       <Text>No entry yet!</Text>
+
+  //     </View>
+  //   );
+  // }
 
   return (
-    <View style={styles.container}>
-      <Text>Cities</Text>
-      <View>{listCities}</View>
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.header_text}>Cities</Text>
+      </View>
+
+      {/* <View>{listCities}</View> */}
+   
     </View>
   );
 }
@@ -27,11 +35,19 @@ export default Cities;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
   empty_container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header_text: {
+    fontSize: 50,
   },
 });
