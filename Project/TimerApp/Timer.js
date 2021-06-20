1c9559ee1e962d79ef9c7c41e0040334a1e86a03
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-20 21:00:13 +0000

    added Timer

---
+++ Project/TimerApp/Timer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet} from "react-native";
+import TimerList from "./TimerList";
+
+const Timer = () => {
+  return <TimerList />;
+};
+
+export default Timer;
+
+const styles = StyleSheet.create({});
